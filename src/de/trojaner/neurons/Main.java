98e2997f5104bdf147b7d0b230d969aa5fbcf466
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2021-05-26 13:45:53 +0200

    Neural Network works for basic input Randomized initial weights between neurons

--- src/de/trojaner/neurons/Main.java
+++ src/de/trojaner/neurons/Main.java
@@ -4,58 +4,48 @@ import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-    /*Path inputPath = Paths.get("inputs");
-    Path correct = inputPath.resolve("true");
-    Path incorrect = inputPath.resolve("false");
-    File[] correctFiles = correct.toFile().listFiles();
-    File[] incorrectFiles = incorrect.toFile().listFiles();
+    /*
+     * Path inputPath = Paths.get("inputs"); Path correct =
+     * inputPath.resolve("true"); Path incorrect = inputPath.resolve("false");
+     * File[] correctFiles = correct.toFile().listFiles(); File[] incorrectFiles =
+     * incorrect.toFile().listFiles();
+     * 
+     * File[] fileInputs = new File[correctFiles.length + incorrectFiles.length];
+     * System.arraycopy(correctFiles, 0, fileInputs, 0, correctFiles.length);
+     * System.arraycopy(incorrectFiles, 0, fileInputs, correctFiles.length,
+     * incorrectFiles.length);
+     */
 
-    File[] fileInputs = new File[correctFiles.length + incorrectFiles.length];
-    System.arraycopy(correctFiles, 0, fileInputs, 0, correctFiles.length);
-    System.arraycopy(incorrectFiles, 0, fileInputs, correctFiles.length, incorrectFiles.length);*/
+    // int[][] inputs = PictureAnalyzer.analyzeFiles(fileInputs);
+    /*
+     * float[][] inputs = { { 0, 0, 1, 0, 1, 0, 1, 0, 0 }, { 0, 1, 1, 1, 1, 1, 1, 1,
+     * 0 }, { 1, 0, 0, 0, 1, 0, 0, 0, 1, }, { 0, 1, 1, 0, 1, 0, 1, 1, 0, } };
+     * NeuralNetwork network = new NeuralNetwork(inputs);
+     * network.learn(List.of(true, true, false, false));
+     */
+    float[][] inputs = { { 1, 0, 0 }, { 1, 1, 0 }, { 1, 1, 1 }, { 0, 1, 1 }, { 0, 0, 0 } };
 
-    //int[][] inputs = PictureAnalyzer.analyzeFiles(fileInputs);
-    float[][] inputs = {
-      {
-        0, 0, 1,
-        0, 1, 0,
-        1, 0, 0
-      },
-      {
-        0, 1, 1,
-        1, 1, 1,
-        1, 1, 0
-      },
-      {
-        1, 0, 0,
-        0, 1, 0,
-        0, 0, 1,
-      },
-      {
-        0, 1, 1,
-        0, 1, 0,
-        1, 1, 0,
-        }
-    };
     NeuralNetwork network = new NeuralNetwork(inputs);
-    network.learn(List.of(true, true, false, false));
+    network.learn(List.of(true, true, true, false, false), 100);
 
+    System.out.println(network.solve(new float[] { 1, 0, 1 }));
+    System.out.println(network.solve(new float[] { 0, 0, 1 }));
 
-
-    /*    List<Neuron> subneurons = new ArrayList();
-          for (int i = 0; i < 8; i++) subneurons.add(new Neuron());
-          Neuron[] subneuronArray = new Neuron[subneurons.size()];
-          subneuronArray = subneurons.toArray(subneuronArray);
-          Neuron mainNeuron = new Neuron(subneuronArray);
-          File inputPath = new File("input");
-          File[] inputs = inputPath.listFiles();
-          PictureInput inputPicture = new PictureInput(inputs, mainNeuron);
-          inputPicture.input();
-          for (Neuron subneuron: mainNeuron.getSubneurons()) System.out.println(subneuron.getWeight());
-          File analyzePath = new File("analyze");
-          File[] analyzeFiles = analyzePath.listFiles();
-          PictureAnalyzer analyzer = new PictureAnalyzer(analyzeFiles, mainNeuron);
-          List<Boolean> results = analyzer.analyze();
-          for (int i = 0; i < analyzeFiles.length; i++) System.out.println(String.format("%s: %s", analyzeFiles[i].getName(), results.get(i)));*/
+    /*
+     * List<Neuron> subneurons = new ArrayList(); for (int i = 0; i < 8; i++)
+     * subneurons.add(new Neuron()); Neuron[] subneuronArray = new
+     * Neuron[subneurons.size()]; subneuronArray =
+     * subneurons.toArray(subneuronArray); Neuron mainNeuron = new
+     * Neuron(subneuronArray); File inputPath = new File("input"); File[] inputs =
+     * inputPath.listFiles(); PictureInput inputPicture = new PictureInput(inputs,
+     * mainNeuron); inputPicture.input(); for (Neuron subneuron:
+     * mainNeuron.getSubneurons()) System.out.println(subneuron.getWeight()); File
+     * analyzePath = new File("analyze"); File[] analyzeFiles =
+     * analyzePath.listFiles(); PictureAnalyzer analyzer = new
+     * PictureAnalyzer(analyzeFiles, mainNeuron); List<Boolean> results =
+     * analyzer.analyze(); for (int i = 0; i < analyzeFiles.length; i++)
+     * System.out.println(String.format("%s: %s", analyzeFiles[i].getName(),
+     * results.get(i)));
+     */
   }
 }
--- src/de/trojaner/neurons/NeuralNetwork.java
+++ src/de/trojaner/neurons/NeuralNetwork.java
@@ -1,12 +1,12 @@
 package de.trojaner.neurons;
 
 import java.util.List;
+import java.util.Random;
 import java.util.ArrayList;
 
 public class NeuralNetwork {
   private float[][] hiddenLayer1Weights;
   private float[][] outputLayerWeights;
-  private int numberInputNeurons = 9;
   private int numberHiddenLayer1Neurons = 9;
   private int numberOutputNeurons = 1;
   private boolean output = false;
@@ -14,61 +14,54 @@ public class NeuralNetwork {
   private List<float[]> hiddenLayer1ValuesPerRun = new ArrayList<>();
 
   public NeuralNetwork(float numberOfHiddenLayers, float[][] inputSet) {
-    this.initWeights();
     this.inputSet = inputSet;
+    this.initWeights();
   }
 
   public NeuralNetwork(float[][] inputSet) {
-    this.initWeights();
     this.inputSet = inputSet;
+    this.initWeights();
   }
 
-  private List<Boolean> predict() {
-    List<Boolean> results = new ArrayList<>();
-    for (float[] inputs : this.inputSet) {
-      float[] hiddenLayer1Values = new float[numberHiddenLayer1Neurons];
-      for (int i = 0; i < inputs.length; i++)
-        for (int j = 0; j < numberHiddenLayer1Neurons; j++)
-          hiddenLayer1Values[j] += inputs[i] * hiddenLayer1Weights[j][i];
+  public boolean solve(float[] inputs) {
+    float[] hiddenLayer1Values = new float[numberHiddenLayer1Neurons];
+    for (int i = 0; i < inputs.length; i++)
+      for (int j = 0; j < numberHiddenLayer1Neurons; j++)
+        hiddenLayer1Values[j] += inputs[i] * hiddenLayer1Weights[j][i];
 
-      hiddenLayer1ValuesPerRun.add(hiddenLayer1Values);
+    hiddenLayer1ValuesPerRun.add(hiddenLayer1Values);
 
-      float[] outputs = new float[numberOutputNeurons];
-      for (int i = 0; i < hiddenLayer1Values.length; i++)
-        for (int j = 0; j < outputs.length; j++)
-          outputs[j] += hiddenLayer1Values[i] * outputLayerWeights[j][i];
+    float[] outputs = new float[numberOutputNeurons];
+    for (int i = 0; i < hiddenLayer1Values.length; i++)
+      for (int j = 0; j < outputs.length; j++)
+        outputs[j] += hiddenLayer1Values[i] * outputLayerWeights[j][i];
 
-      results.add(outputs[0] >= STATIC.THRESHOLD);
+    return outputs[0] >= STATIC.THRESHOLD;
 
-    }
-    return results;
   }
 
-  public void learn(List<Boolean> expectedOutputs) {
-    for (int temp = 0; temp < 10; temp++) {
-      System.out.println("Temp: " + temp);
-      List<Boolean> results = this.predict();
+  public void learn(List<Boolean> expectedOutputs, int iterations) {
+    for (int iteration = 0; iteration < iterations; iteration++) {
+      System.out.println("Iteration: " + iteration);
+      List<Boolean> results = new ArrayList<>();
+      for (float[] inputs : this.inputSet)
+        results.add(this.solve(inputs));
       for (int i = 0; i < results.size(); i++) {
         System.out.println(String.format("Index: %d", i));
-        boolean result = results.get(i); // false
-        boolean expectedOutput = expectedOutputs.get(i); // true
+        boolean result = results.get(i);
+        boolean expectedOutput = expectedOutputs.get(i);
         if (result == expectedOutput)
-          continue; // 0
+          continue; // outputDelta == 0
 
-        float[] hiddenLayer1Values = this.hiddenLayer1ValuesPerRun.get(i); // 0, 0, 0, 0, 0, 0, 0
+        float[] hiddenLayer1Values = this.hiddenLayer1ValuesPerRun.get(i);
 
-        float[] input = this.inputSet[i]; // 0, 0, 1, 0, 1, 0, 1, 0, 0
+        float[] input = this.inputSet[i];
 
-        /*
-         * if (result && !expected) outputDelta = 1; else if (!result && expected)
-         * outputDelta = -1;
-         */
         float outputDelta = result ? 1 : -1;
         float[] hiddenLayerDeltas = new float[this.numberHiddenLayer1Neurons]; // new int[9]
-        for (int j = 0; j < this.outputLayerWeights[0].length; j++) {
+        for (int j = 0; j < this.outputLayerWeights[0].length; j++)
           hiddenLayerDeltas[j] = outputDelta * this.outputLayerWeights[0][j];
-        }
-        // hiddenLayerDelta = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        
         this.changeWeights(this.hiddenLayer1Weights, hiddenLayerDeltas, input); // hiddenLayer1Weights = input
         this.changeWeights(this.outputLayerWeights, new float[] { outputDelta }, hiddenLayer1Values); //
         System.out.println(String.format("Output Error: %s%nHidden layer weights:", outputDelta));
@@ -82,9 +75,8 @@ public class NeuralNetwork {
   private void changeWeights(float[][] weights, float[] deltas, float[] input) {
     for (int i = 0; i < weights.length; i++) {
       float delta = deltas[i];
-      for (int j = 0; j < weights[i].length; j++) {
-        weights[i][j] -= STATIC.SCALE * delta * input[j] * 0.5;
-      }
+      for (int j = 0; j < weights[i].length; j++)
+        weights[i][j] -= STATIC.SCALE * delta * input[j];
     }
   }
 
@@ -97,16 +89,17 @@ public class NeuralNetwork {
   }
 
   private void initWeights() {
-    this.hiddenLayer1Weights = new float[this.numberHiddenLayer1Neurons][this.numberInputNeurons];
+    this.hiddenLayer1Weights = new float[this.numberHiddenLayer1Neurons][this.inputSet[0].length];
     this.outputLayerWeights = new float[this.numberOutputNeurons][this.numberHiddenLayer1Neurons];
+    Random r = new Random();
     for (int i = 0; i < hiddenLayer1Weights.length; i++) {
       for (int j = 0; j < hiddenLayer1Weights[i].length; j++) {
-        hiddenLayer1Weights[i][j] = 1;
+        hiddenLayer1Weights[i][j] = r.nextFloat() * 2 - 1;
       }
     }
     for (int i = 0; i < outputLayerWeights.length; i++) {
       for (int j = 0; j < outputLayerWeights[i].length; j++) {
-        outputLayerWeights[i][j] = 1;
+        outputLayerWeights[i][j] = r.nextFloat() * 2 - 1;
       }
     }
   }
